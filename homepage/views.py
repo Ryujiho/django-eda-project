a6abc70ad3a9d087b9d75e726f05a6945a2e64dd
Jiho, Yoo <goll167@gmail.com>
2022-01-06 23:01:42 +0900

    Apply Bootstrap(css,js,html)

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render, HttpResponse
+import pandas as pd
+import os
+from pathlib import Path
 
 # Create your views here.
 def index(request):
-    return render(request, 'eda.html', {})
+
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    path = os.path.join(BASE_DIR, 'homepage', 'static', 'alcohol.csv')
+
+    contents={}
+    contents['csv_df'] = pd.read_csv(path)
+    return render(request, 'index.html', contents)
